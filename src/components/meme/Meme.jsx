9ba94c957740b6a010ff8ebba535b9bbb4a62aea
Mark <muchirimark2@gmail.com>
2023-08-29 08:16:02 +0300

    added hover effect and small changes

--- src/components/meme/Meme.jsx
+++ src/components/meme/Meme.jsx
@@ -4,8 +4,8 @@ export default function Meme() {
     return (
         <>
             <div className="meme-image-container">
-                <p className='top-text'>SHUT UP</p>
                 <img className='meme-img' src="images/memeimg.png" alt="Meme image generated" />
+                <p className='top-text'>SHUT UP</p>
                 <p className='bottom-text'>AND TAKE MY MONEY</p>
             </div>
         </>
