e6729e4dc1873bea75c81a9e835af0ca07993a07
Mark <muchirimark2@gmail.com>
2023-08-30 03:13:15 +0300

    random meme onClick function

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,6 +1,22 @@
 import "./Form.css";
+import memeData from "../../data//memesData";
 
 export default function Form() {
+
+    /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     *
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
+    function randomMeme() {
+        const memes = memeData.data.memes;
+        const random_meme = memes[Math.floor(Math.random() * memes.length)];
+        const url = random_meme.url;
+        console.log(url);
+    }
+
     return (
         <>
             <div className="form-section">
@@ -10,7 +26,7 @@ export default function Form() {
                     <input type="text" placeholder="and take my money" />
                 </div>
                 <div className="button">
-                    <button>
+                    <button onClick={randomMeme} >
                         <p>Get a new meme image 🏞️</p>
                     </button>
                 </div>
--- src/components/meme/Meme.jsx
+++ src/components/meme/Meme.jsx
@@ -1,12 +1,17 @@
-import './Meme.css';
+import "./Meme.css";
 
 export default function Meme() {
+
     return (
         <>
             <div className="meme-image-container">
-                <img className='meme-img' src="images/memeimg.png" alt="Meme image generated" />
-                <p className='top-text'>SHUT UP</p>
-                <p className='bottom-text'>AND TAKE MY MONEY</p>
+                <img
+                    className="meme-img"
+                    src="images/memeimg.png"
+                    alt="Meme image generated"
+                />
+                <p className="top-text">SHUT UP</p>
+                <p className="bottom-text">AND TAKE MY MONEY</p>
             </div>
         </>
     );
