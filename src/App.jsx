bd3374c3b8b0ac9ef38f2fa0c3b9590782f31f44
Mark <muchirimark2@gmail.com>
2023-09-03 22:02:03 +0300

    meme pushed to live

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import './App.css';
-import StateFromChild from './components/States/StateFromChild/StateFromChild';
+// import StateFromChild from './components/States/StateFromChild/StateFromChild';
 // import PassingStateAsProps from './components/States/PassingStateAsProps/PassingStateAsProps';
 // import StateTenary from './components/States/StateTenary/StateTenary';
 // import ComplexStateArrays from './components/States/ComplexStateArrays/ComplexStateArrays';
 // import StateTenary from './components/States/StateTenary/StateTenary';
 // import StateCounter from './components/States/stateCounter/StateCounter';
-// import Form from './components/form/Form';
-// import Navbar from './components/navbar/Navbar';
+import Form from './components/form/Form';
+import Navbar from './components/navbar/Navbar';
 
 
 function App() {
 
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <Form /> */}
+      <Navbar />
+      <Form />
       {/* <StateCounter /> */}
       {/* <StateTenary /> */}
       {/* <ComplexStateArrays /> */}
       {/* <ComplexStateObjects /> */}
       {/* <PassingStateAsProps /> */}
-      <StateFromChild />
+      {/* <StateFromChild /> */}
     </>
   );
 }
