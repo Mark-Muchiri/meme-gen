ac17dc4f5763a971844794820511a6cebc4139c4
Mark <muchirimark2@gmail.com>
2023-09-04 22:08:20 +0300

    - box color challenge done

---
+++ src/components/challenge/box.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import propTypes from 'prop-types';
+
+export default function Box(props) {
+    Box.propTypes = {
+        on: propTypes.bool,
+    };
+
+    const [onValue, setOnValue] = useState(props.on);
+
+    const styles = {
+        borderRadius: '0.3em',
+        width: '100px',
+        height: '100px',
+        padding: '0',
+        // onValue style
+        backgroundColor: onValue ? '#0f111a' : 'lightslategrey',
+        border: onValue ? '1px solid darkslategray' : '1px solid white'
+    };
+
+    function colorSwitch() {
+        setOnValue(color => !color);
+    }
+    return (
+        <div onClick={colorSwitch} style={styles} className="box"></div>
+    );
+}
--- src/components/challenge/challenge.jsx
+++ src/components/challenge/challenge.jsx
@@ -1,12 +1,16 @@
 import './challenge.css';
 import boxes from './boxes';
 import { useState } from 'react';
-import propTypes from 'prop-types';
+import Box from './box';
 
-export default function Challenge(props) {
-    Challenge.propTypes = {
-        darkmode: propTypes.bool.isRequired,
-    };
+export default function Challenge() {
+    const [data] = useState(boxes);
+
+    // box map func
+    const boxItems = data.map(items => (
+        <Box key={items.id} on={items.on} />
+    ));
+
     /**
      * Challenge part 2:
      * 1. Create a separate component called "Box" and
@@ -17,31 +21,14 @@ export default function Challenge(props) {
      *    the backgroundColor of the box. If it's `on`, set the
      *    backgroundColor to "#222222". If off, set it to "none"
      */
-    const [data, setData] = useState(boxes);
-    const [theme, setTheme] = useState(props.darkmode);
 
-    function themeSwitch() {
-        setTheme(color => !color);
-    }
-
-    function emptyState() {
-        setData();
-    }
-
-    const styles = {
-        backgroundColor: theme ? '#0f111a' : 'lightslategrey',
-        border: theme ? '1px solid darkslategray' : '1px solid white'
-    };
-
-    // box map fn
-    const boxItems = data.map(items => (
-        <div style={styles} className='box' key={items.id}></div>
-    ));
     return (
         <main>
-            <h1 onClick={emptyState}>Boxes will go here</h1>
-            <div onClick={themeSwitch} className='boxes'>
-                {boxItems}
+            <h1>Boxes will go here</h1>
+            <div className="pos">
+                <div className='boxes'>
+                    {boxItems}
+                </div>
             </div>
         </main>
     );
