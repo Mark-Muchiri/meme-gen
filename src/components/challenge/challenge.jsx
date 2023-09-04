500263fc8ed98393ee303e882a823efc05329913
Mark <muchirimark2@gmail.com>
2023-09-05 01:55:55 +0300

    boxes part 3.2

--- src/components/challenge/challenge.jsx
+++ src/components/challenge/challenge.jsx
@@ -5,21 +5,17 @@ import Box from './box';
 
 export default function Challenge() {
     const [data] = useState(boxes);
-
     // box map func
     const boxItems = data.map(items => (
         <Box key={items.id} on={items.on} />
     ));
 
     /**
-     * Challenge part 2:
-     * 1. Create a separate component called "Box" and
-     *    replace the `div` above with our <Box /> components
-     * 2. Pass the Box component a prop called `on` with the
-     *    value of the same name from the `boxes` objects
-     * 3. In the Box component, apply dynamic styles to determine
-     *    the backgroundColor of the box. If it's `on`, set the
-     *    backgroundColor to "#222222". If off, set it to "none"
+     * Challenge: Create a toggle() function that logs
+     * "clicked!" to the console
+     * 
+     * Pass that function down to each of the Box components
+     * and set it up so when they get clicked it runs the function
      */
 
     return (
