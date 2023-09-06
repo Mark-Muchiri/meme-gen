de2ae6ec78d80bec827c128d8523e37ee48e0049
Mark <muchirimark2@gmail.com>
2023-09-06 03:55:36 +0300

    1st Conditional rendering exercise done

--- src/components/Conditional rendering: &&/Jokes.jsx
+++ src/components/Conditional rendering: &&/Jokes.jsx
@@ -1,19 +1,37 @@
 import propTypes from 'prop-types'
+import { useState } from 'react';
 
 export default function Joke(props) {
     Joke.propTypes = {
         setup: propTypes.string,
         punchline: propTypes.string,
     }
+    const styles = {
+        marginBottom: '10px',
+        border: '1px solid',
+        borderRadius: '5px',
+        paddingTop: '3px',
+        paddingBottom: '4px',
+        paddingLeft: '13px',
+        paddingRight: '13px',
+    }
         /**
      * Challenge:
      * - Create state `isShown` (boolean, default to `false`)
      * - Add a button that toggles the value back and forth
      */
+    const [isShown, setIsShown] = useState(false)
+    console.log(isShown);
+    function showSwitch() {
+        setIsShown(prevIsShown => {
+            return prevIsShown = !prevIsShown
+        })
+    }
         return (
             <div>
                 {props.setup && <h3>{props.setup}</h3>}
                 <p>{props.punchline}</p>
+                <button onClick={showSwitch} style={styles}>Toggle</button>
                 <div className='line' />
             </div>
         )
