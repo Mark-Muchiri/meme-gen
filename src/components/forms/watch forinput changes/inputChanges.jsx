77edd4e731c4b3dd89c17167c3310c23131c1d10
Mark <muchirimark2@gmail.com>
2023-09-08 02:37:59 +0300

    - Learned how to do forms with multiple inputs

--- src/components/forms/watch forinput changes/inputChanges.jsx
+++ src/components/forms/watch forinput changes/inputChanges.jsx
@@ -1,24 +1,40 @@
 import { useState } from 'react';
-import './inputChanges.css'
-
+import './inputChanges.css';
 
 export default function InputChanges() {
-    const [firstName, setFirstName] = useState("")
-    
+    const [formData, setFormData] = useState(
+        { firstName: "", lastName: "" }
+    );
     function handleChange(event) {
         /**
-         * Challenge: update the firstName state on every keystroke
-         */
-        setFirstName(event.target.value)
+        * Challenge: Track the applicant's last name as well
+        */
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            };
+        });
     }
     return (
-        <form>
-            <input
-                type="text"
-                placeholder="First Name"
-                onChange={handleChange}
-            />
-            <p>{firstName}</p>
-        </form>
-    )
+        <section>
+            <form>
+                <input
+                    name='firstName'
+                    type="text"
+                    placeholder="First Name"
+                    onChange={handleChange}
+                />
+                <input
+                    name='lastName'
+                    type="text"
+                    placeholder="Last Name"
+                    onChange={handleChange}
+                />
+            </form>
+            <div className="center">
+                <p>Hello {formData.firstName} {formData.lastName} 😃</p>
+            </div>
+        </section>
+    );
 }
