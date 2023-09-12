29cdda44b5d422fe483f5c39f4749448855198ff
Mark <muchirimark2@gmail.com>
2023-09-12 05:51:25 +0300

    Learned about useEffect hook
    Did an exercise on it to understand it

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import UseEffectDep from './components/useEffect/UseEffectDep'
 // import Formprac from './components/forms/FormPrac/Formprac';
 // import InputChanges from './components/forms/inputChanges';
 // import AndPractice from './components/Conditional rendering: &&/practice/AndPractice'
@@ -11,16 +12,16 @@ import './App.css';
 // import ComplexStateArrays from './components/States/ComplexStateArrays/ComplexStateArrays';
 // import StateTenary from './components/States/StateTenary/StateTenary';
 // import StateCounter from './components/States/stateCounter/StateCounter';
-import Meme from './components/form/Meme';
-import Navbar from './components/navbar/Navbar';
+// import Meme from './components/form/Meme';
+// import Navbar from './components/navbar/Navbar';
 
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Meme />
+      {/* <Navbar /> */}
+      {/* <Meme /> */}
       {/* <StateCounter /> */}
       {/* <StateTenary /> */}
       {/* <ComplexStateArrays /> */}
@@ -33,6 +34,7 @@ function App() {
       {/* <AndPractice /> */}
       {/* <InputChanges /> */}
       {/* <Formprac /> */}
+      <UseEffectDep />
     </>
   );
 }
---
+++ src/components/useEffect/UseEffectDep.jsx
@@ -0,0 +1,24 @@
+import './useEffectDep.css';
+import { useEffect, useState } from 'react';
+
+export default function useEffectDep() {
+    // const [starWarsData, setStarWarsData] = useState({})
+    const [count, setCount] = useState(0)
+    
+    console.log("Component rendered")
+    /**
+     * Challenge: re-write the useEffect
+     * It should run any time `count` changes
+     * For now, just console.log("Effect function ran")
+     */
+    useEffect(function () {
+        console.log(`Effect function run`)
+    }, [count])
+    
+    return (
+        <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+        </div>
+    )
+}
