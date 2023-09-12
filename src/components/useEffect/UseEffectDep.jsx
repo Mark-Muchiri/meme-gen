54511e4465d0e6a8df35311b2be84c94119e84d5
Mark <muchirimark2@gmail.com>
2023-09-12 23:48:42 +0300

    none

--- src/components/useEffect/UseEffectDep.jsx
+++ src/components/useEffect/UseEffectDep.jsx
@@ -5,14 +5,14 @@ export default function useEffectDep() {
     // const [starWarsData, setStarWarsData] = useState({})
     const [count, setCount] = useState(0);
 
-    console.log("Component rendered");
+    // console.log("Component rendered");
     /**
      * Challenge: re-write the useEffect
      * It should run any time `count` changes
      * For now, just console.log("Effect function ran")
      */
     useEffect(function () {
-        console.log(`Effect function run`);
+        // console.log(`Effect function run`);
     }, [count]);
 
     return (
