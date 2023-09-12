0cbb7fbdd5c0f88ab79d36dd48ce2cb3bfd1fe29
Mark <muchirimark2@gmail.com>
2023-09-13 00:45:28 +0300

    Added a useEffect challenge

--- src/components/useEffect/UseEffectDep.jsx
+++ src/components/useEffect/UseEffectDep.jsx
@@ -5,6 +5,14 @@ export default function useEffectDep() {
     const [starWarsData, setStarWarsData] = useState({});
     const [count, setCount] = useState(0);
 
+    /**
+ * Challenge: Combine `count` with the request URL
+ * so pressing the "Get Next Character" button will
+ * get a new character from the Star Wars API.
+ * Remember: don't forget to consider the dependencies
+ * array!
+ */
+
     useEffect(function () {
         fetch("https://swapi.dev/api/people/1")
             .then(res => res.json())
@@ -13,9 +21,9 @@ export default function useEffectDep() {
 
     return (
         <div>
-            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
             <h2>The count is {count}</h2>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     );
 }
