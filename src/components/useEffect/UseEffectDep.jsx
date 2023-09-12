14edd01e6a6c3ab64eb2a2f4f7ad242166204ac9
Mark <muchirimark2@gmail.com>
2023-09-13 00:11:22 +0300

    none

--- src/components/useEffect/UseEffectDep.jsx
+++ src/components/useEffect/UseEffectDep.jsx
@@ -2,21 +2,18 @@ import './useEffectDep.css';
 import { useEffect, useState } from 'react';
 
 export default function useEffectDep() {
-    // const [starWarsData, setStarWarsData] = useState({})
+    const [starWarsData, setStarWarsData] = useState({});
     const [count, setCount] = useState(0);
 
-    // console.log("Component rendered");
-    /**
-     * Challenge: re-write the useEffect
-     * It should run any time `count` changes
-     * For now, just console.log("Effect function ran")
-     */
     useEffect(function () {
-        // console.log(`Effect function run`);
-    }, [count]);
+        fetch("https://swapi.dev/api/people/1")
+            .then(res => res.json())
+            .then(data => setStarWarsData(data));
+    }, []);
 
     return (
         <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
             <h2>The count is {count}</h2>
             <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
         </div>
