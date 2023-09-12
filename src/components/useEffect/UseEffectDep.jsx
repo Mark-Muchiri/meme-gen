83a1d146cb244989f0a3958517a896609f214eaa
Mark <muchirimark2@gmail.com>
2023-09-12 14:34:30 +0300

    nonne

--- src/components/useEffect/UseEffectDep.jsx
+++ src/components/useEffect/UseEffectDep.jsx
@@ -3,22 +3,22 @@ import { useEffect, useState } from 'react';
 
 export default function useEffectDep() {
     // const [starWarsData, setStarWarsData] = useState({})
-    const [count, setCount] = useState(0)
-    
-    console.log("Component rendered")
+    const [count, setCount] = useState(0);
+
+    console.log("Component rendered");
     /**
      * Challenge: re-write the useEffect
      * It should run any time `count` changes
      * For now, just console.log("Effect function ran")
      */
     useEffect(function () {
-        console.log(`Effect function run`)
-    }, [count])
-    
+        console.log(`Effect function run`);
+    }, [count]);
+
     return (
         <div>
             <h2>The count is {count}</h2>
             <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
         </div>
-    )
+    );
 }
